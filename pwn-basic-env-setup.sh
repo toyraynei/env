5c09c7beb102915f715213dc41c71f7c6cbb60f8
toyraynei <toyraynei@gmail.com>
2019-12-02 11:34:36 +0800

    env.sh

---
+++ pwn-basic-env-setup.sh
@@ -0,0 +1,10 @@
+sudo apt install --upgrade;
+sudo apt install update;
+sudo apt install vim -y;
+sudo apt install git -y;
+git clone https://github.com/longld/peda.git ~/peda;
+echo "source ~/peda/peda.py" >> ~/.gdbinit;
+git clone https://github.com/scwuaptx/Pwngdb.git 
+cp ~/Pwngdb/.gdbinit ~/
+sudo apt install python-pip -y;
+pip install pwntools --user;
