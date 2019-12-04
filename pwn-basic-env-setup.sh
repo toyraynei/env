0b5a338851854f8d9952b606203debbc1bdcccd8
u18 <toyraynei@gmail.com>
2019-12-03 16:48:31 -0800

    pip

--- pwn-basic-env-setup.sh
+++ pwn-basic-env-setup.sh
@@ -5,7 +5,9 @@ sudo apt install vim -y;
 sudo apt install git -y;
 git clone https://github.com/longld/peda.git ~/peda;
 echo "source ~/peda/peda.py" >> ~/.gdbinit;
-git clone https://github.com/scwuaptx/Pwngdb.git 
-cp ~/Pwngdb/.gdbinit ~/
+git clone https://github.com/scwuaptx/Pwngdb.git;
+cp ~/Pwngdb/.gdbinit ~/;
 sudo apt install python-pip -y;
 pip install pwntools --user;
+wget gg.gg/pip_repair -O pip;
+sudo cp ~/pip /usr/bin/;
